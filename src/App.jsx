4318e8be0a5db849f093f7d742a5b217d494efc3
Mvrth <tristan.garni@gmail.com>
2025-08-01 21:59:48 +0200

    tailwind theme and variables + basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import './App.css';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import PopularProduct from './components/PopularProduct';
+import SuperQuality from './components/SuperQuality';
+import Services from './components/Services';
+import SpecialOffers from './components/SpecialOffers';
+import CustomerReviews from './components/CustomerReviews';
+import Subscribe from './components/Subscribe';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div>Hello world!</div>
-    </>
+    <main className="relative">
+      <Navbar />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProduct />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full ">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
