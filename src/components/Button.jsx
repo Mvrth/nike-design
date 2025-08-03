983986cb5be2b7b3441a5e9de7ee3214a64693b8
Mvrth <tristan.garni@gmail.com>
2025-08-03 17:44:50 +0200

    WIP Customer Reviews Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border-color-red font-montserrat text-lg leading-none bg-coral-red rounded-full text-white ">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none  rounded-full border-1 ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'text-white bg-coral-red  border-coral-red'
+      }`}
+    >
       {label}
       {iconURL && (
         <img src={iconURL} alt={label} className="ml-2 rounded-full w-5 h-5" />
